cf5cf364ae9cefe6b8963a136933a059393b08cd
JUDHI3134 <judhistirbehera241@gmail.com>
2024-03-02 10:06:41 +0530

    complete hero and About section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import styles from './App.module.css'
 import Navbar from './Components/Navbar/Navbar'
+import Hero from './Components/Hero/Hero'
+import { About } from './Components/About/About'
 
 const App = () => {
   return (
     <div className={styles.App}>
       <Navbar/>
+      <Hero/>
+      <About/>
     </div>
   )
 }
