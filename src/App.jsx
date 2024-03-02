3cb901f3a15fa8de902d08af5b914fe68eae7270
JUDHI3134 <judhistirbehera241@gmail.com>
2024-03-02 09:02:35 +0530

    create and complete navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import styles from './App.module.css'
+import Navbar from './Components/Navbar/Navbar'
 
 const App = () => {
   return (
-    <div className={App}>
-      Hello World
+    <div className={styles.App}>
+      <Navbar/>
     </div>
   )
 }
